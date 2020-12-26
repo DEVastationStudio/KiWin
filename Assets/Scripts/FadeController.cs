ba3e80cce35c3a946c244e5f7c8cb1d49371a5c3
Zeus guy <Zeusguy@hotmail.es>
2020-12-26 16:37:59 +0100

    Implemented fade transition between scenes

---
+++ Assets/Scripts/FadeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FadeController : MonoBehaviour
+{
+    private string _targetScene = "SampleScene";
+    public Animator animator;
+
+    public void BeginFade(string targetScene)
+    {
+        _targetScene = targetScene;
+        animator.SetTrigger("Fade");
+    }
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(_targetScene);
+    }
+}
