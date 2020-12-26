083b858827e667f8f0daf70e1b857de48147c80f
plpgamedev <plpgamedev@gmail.com>
2020-12-26 18:18:06 +0100

    Main Menu Added

---
+++ Assets/LobbyControllerScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyControllerScript : MonoBehaviour
+{
+
+    [SerializeField] private Button StartBtn;
+    [SerializeField] private Image ReadyAnimal;
+    [SerializeField] private Image ReadyFruta;
+
+    private bool isAnimalReady;
+    private bool isFrutaReady;
+
+    public void Start()
+    {
+        StartBtn.gameObject.SetActive(false);
+        ReadyFruta.gameObject.SetActive(false);
+        ReadyAnimal.gameObject.SetActive(false);
+    }
+
+    public void ReadyAnimalBtn() 
+    {
+        isAnimalReady = !isAnimalReady;
+        ReadyAnimal.gameObject.SetActive(isAnimalReady);
+        startGame();
+    }
+
+    public void ReadyFrutaBtn()
+    {
+        isFrutaReady = !isFrutaReady;
+        ReadyFruta.gameObject.SetActive(isFrutaReady);
+        startGame();
+    }
+
+    private void startGame() 
+    {
+        StartBtn.gameObject.SetActive(isAnimalReady && isFrutaReady);
+    }
+
+    public void ResetLobby() 
+    {
+        isAnimalReady = false;
+        isFrutaReady = false;
+        ReadyAnimal.gameObject.SetActive(false);
+        ReadyFruta.gameObject.SetActive(false);
+        StartBtn.gameObject.SetActive(false);
+    }
+
+}
---
+++ Assets/MusicScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicScript : MonoBehaviour
+{
+    [SerializeField] private Sprite musicOnImg;
+    [SerializeField] private Sprite musicOffImg;
+    [SerializeField] private Image btnImg;
+
+    private bool isMusicOn = true;
+
+    public void MusicButton() 
+    {
+        isMusicOn = !isMusicOn;
+        if (isMusicOn)
+            btnImg.sprite = musicOnImg;
+        else
+            btnImg.sprite = musicOffImg;
+
+
+
+    }
+}
