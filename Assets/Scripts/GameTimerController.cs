54f80b5a7c17829296f0f4a93a3d607a9d59d10a
Zeus guy <Zeusguy@hotmail.es>
2020-12-26 17:52:50 +0100

    Implemented in-game timer

---
+++ Assets/Scripts/GameTimerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimerController : MonoBehaviour
+{
+    public delegate void TimerEndDelegate();
+    public TimerEndDelegate OnTimerEnd;
+    public Text timerText;
+    public bool autoStart = false;
+    private float _timer = 0;
+
+    void Start()
+    {
+        OnTimerEnd += StopTimer;
+        if (autoStart)
+        {
+            BeginTimer(90);
+        }
+        else
+        {
+            FormatTimer();
+        }
+    }
+
+    void Update()
+    {
+        if (_timer > 0)
+        {
+            _timer -= Time.deltaTime;
+            FormatTimer();
+            if (_timer <= 0)
+            {
+                OnTimerEnd();
+            }
+        }
+    }
+
+    public void BeginTimer(int seconds)
+    {
+        _timer = seconds;
+        FormatTimer();
+    }
+
+    private void FormatTimer()
+    {
+        timerText.text = string.Format("{0:00}", Mathf.Floor(_timer/60)) + ":" + string.Format("{0:00}", Mathf.Floor(_timer%60));
+    }
+
+    private void StopTimer()
+    {
+        timerText.text = "00:00";
+        this.enabled = false;
+    }
+
+}
---
+++ Assets/Test Scripts/TimerTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerTest : MonoBehaviour
+{
+    public GameTimerController timerController;
+    public FadeController fadeController;
+
+    public void SetupTimer()
+    {
+        timerController.OnTimerEnd += EndTimer;
+        timerController.BeginTimer(9);
+    }
+
+    private void EndTimer() {
+        Debug.Log("Scene Restarted");
+        fadeController.BeginFade("SampleScene");
+    }
+}
