4508002e2f162dd0a0b541df060ffb1c055cffe4
tomas <tomas22tomas@gmail.com>
2020-12-27 16:43:14 +0100

    ColisionesConFrutas

---
+++ Assets/Scripts/FrutaNoMueve.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrutaNoMueve : MonoBehaviour
+{
+    public Renderer ren;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        ren = GetComponent<Renderer>();
+        ren.material.SetColor("_Color", Color.green);
+    }
+}
